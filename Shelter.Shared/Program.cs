4c411a18efcc56ffa4c0b61123742adcb0df5f02
Angelique <angeliquebuijzen@hotmail.com>
2019-10-02 16:31:01 +0200

    startfiles

---
+++ Shelter.Shared/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shelter.Shared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
