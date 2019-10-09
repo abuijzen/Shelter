2f56fd98a4e4ea23658467910e86218dcb0efa2c
Elkeborr <elkeborreij@student.thomasmore.be>
2019-10-09 16:49:27 +0200

    controller_base

--- Shelter.Mvc/Controllers/ShelterController.cs
+++ Shelter.Mvc/Controllers/ShelterController.cs
@@ -1,2 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shelter.Mvc.Models;
 
+namespace Shelter.Mvc.Controllers
+{
+    public class ShelterController : Controller
+    {
+
+    }
+}
