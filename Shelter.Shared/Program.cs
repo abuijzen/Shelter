2b6824f2b7c59c1a73727a5cb94c161956000987
Elkeborr <elkeborreij@student.thomasmore.be>
2019-10-09 16:41:34 +0200

    Make_Rabbit1

--- Shelter.Shared/Program.cs
+++ Shelter.Shared/Program.cs
@@ -8,8 +8,10 @@ namespace Shelter.Shared
 		{
 
 			Cat cat1 = new Cat(1, "Felix", "Britse Korthaar", new DateTime(2005, 10, 09), false, true, true, true, new DateTime(2007, 10, 09), "meow I'm a cat", "catnip", true);
-			Console.WriteLine("Hello World!");
+			Rabbit rabbit1 = new Rabbit(2, "Ior", "Hollander", new DateTime(2017, 12, 25), false, true, true, true, new DateTime(2018, 09, 10), "Ior is een cutiepie", "nothing", "small");
+            Console.WriteLine("Hello World!");
 			Console.WriteLine(cat1.Name);
+            Console.WriteLine(rabbit1.Name);
 		}
 	}
 }
