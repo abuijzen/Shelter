8e726ac4130b0cae13ecb4702689c4a750a229e3
haeirene <haegeman.irene@hotmail.com>
2019-10-16 17:08:33 +0200

    Add function delete - Create a delete page for each animal

--- Shelter.Mvc/Controllers/ShelterController.cs
+++ Shelter.Mvc/Controllers/ShelterController.cs
@@ -10,25 +10,35 @@ using Shelter.Shared;
 
 namespace Shelter.Mvc.Controllers
 {
-    public class ShelterController : Controller
-    {
-        private readonly ILogger<ShelterController> _logger;
+	public class ShelterController : Controller
+	{
+		private readonly ILogger<ShelterController> _logger;
 
-        public ShelterController(ILogger<ShelterController> logger)
-        {
-            _logger = logger;
-        }
+		public ShelterController(ILogger<ShelterController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            // opbouwen van het view model en in het view model steken
+		public IActionResult Index()
+		{
+			// opbouwen van het view model en in het view model steken
 
 
-        var cat1 =  new Cat(1, "Felix", "Britse Korthaar", new DateTime(2005, 10, 09), false, true, true, true, new DateTime(2007, 10, 09), "meow I'm a cat", "catnip", true);
-        var cat2 =   new Cat(2, "Picasso", "Ragdoll", new DateTime(2010, 03, 10), false, true, true, true, new DateTime(2011, 08, 10), "Mieeeuw", "dogs", true);
-        var rabbit1 =  new Rabbit(2, "Ior", "Hollander", new DateTime(2017, 12, 25), false, true, true, true, new DateTime(2018, 09, 10), "Ior is een cutiepie", "nothing", "small");
+			var cat1 = new Cat(1, "Felix", "Britse Korthaar", new DateTime(2005, 10, 09), false, true, true, true, new DateTime(2007, 10, 09), "meow I'm a cat", "catnip", true);
+			var cat2 = new Cat(2, "Picasso", "Ragdoll", new DateTime(2010, 03, 10), false, true, true, true, new DateTime(2011, 08, 10), "Mieeeuw", "dogs", true);
+			var rabbit1 = new Rabbit(2, "Ior", "Hollander", new DateTime(2017, 12, 25), false, true, true, true, new DateTime(2018, 09, 10), "Ior is een cutiepie", "nothing", "small");
 
-        return View(new AnimalViewModel {  Animals = new List<Animal> { cat1, rabbit1, cat2} });
-        }
-    }
+			return View(new AnimalViewModel { Animals = new List<Animal> { cat1, rabbit1, cat2 } });
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var currentAnimal = ShelterController.Shelter.Animals.FirstOrDefault(x => x.Id == id);
+			if (currentAnimal == default(Beer))
+			{
+				return NotFound();
+			}
+			return View(currentAnimal);
+		}
+	}
 }
--- Shelter.Mvc/Models/AnimalViewModel.cs
+++ Shelter.Mvc/Models/AnimalViewModel.cs
@@ -4,8 +4,8 @@ using Shelter.Shared;
 
 namespace Shelter.Mvc.Models
 {
-    public class AnimalViewModel
-    {
-        public List<Animal> Animals { get; set; }
-    }
+	public class AnimalViewModel
+	{
+		public List<Animal> Animals { get; set; }
+	}
 }
