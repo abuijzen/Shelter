60d78c5fb77f4153127a5b50be42126e16e312fa
haeirene <haegeman.irene@hotmail.com>
2019-11-20 16:11:17 +0100

    Add methods for shelter specific information

--- Shelter.Shared/Shelter.cs
+++ Shelter.Shared/Shelter.cs
@@ -5,6 +5,12 @@ namespace Shelter.Shared
 {
 	public class Shelter
 	{
+		public string Id { get; set; }
+		public string name { get; set; }
+		public string imageUrl { get; set; }
+		public string address { get; set; }
+		public string telephoneNumber { get; set; }
+		public string emailAdress { get; set; }
 		public List<Animal> Animals { get; set; }
 		public List<Manager> Managers { get; set; }
 		public List<Administrator> Administrators { get; set; }
