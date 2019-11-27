5fa1649dfd37487bcc3560774170187d7102d6a4
Elkeborr <elkeborreij@student.thomasmore.be>
2019-11-27 16:55:29 +0100

    Bunny done

--- Shelter.Shared/DatabaseInitializer.cs
+++ Shelter.Shared/DatabaseInitializer.cs
@@ -49,7 +49,7 @@ namespace Shelter.Shared
 				Animals = new List<Animal> {
 						new Cat(1, "Felix", "Britse Korthaar", new DateTime(2005, 10, 09), false, true, true, true, new DateTime(2007, 10, 09), "meow I'm a cat", "catnip", true),
 						new Cat(2, "Picasso", "Ragdoll", new DateTime(2010, 03, 10), false, true, true, true, new DateTime(2011, 08, 10), "Mieeeuw", "dogs", true),
-						new Rabbit(3, "Ior", "Hollander", new DateTime(2017, 12, 25), false, true, true, true, new DateTime(2018, 09, 10), "Ior is een cutiepie", "nothing", "small"),
+						new Rabbit{Name = "Ior", Race="Hollander", DateOfBirth= new DateTime(2017, 12, 25), IsFertile =false, IsKidFriendly= true, IsAnimalFriendly =true, IsSpeciesFriendly=true, Since=new DateTime(2018, 09, 10), Bio="Ior is een cutiepie", Allergies="none", Size="small"},
 					}
 			};
 			_context.Shelters.Add(shelter);
