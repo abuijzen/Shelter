c172e0fdb0be66d24aa33061993cba6eb28835c4
haeirene <haegeman.irene@hotmail.com>
2019-12-04 16:03:23 +0100

    Bugfix api

--- Shelter.Mvc/Controllers/ApiController.cs
+++ Shelter.Mvc/Controllers/ApiController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Shelter.Mvc.Models;
 using Shelter.Shared;
 using System.Collections.Generic;
 using System.Linq;
