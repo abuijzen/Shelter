59c337267714ef539f55254939d9def850d99135
haeirene <haegeman.irene@hotmail.com>
2019-12-04 15:39:40 +0100

    Add unit tests

---
+++ Shelter.UnitTests/UnitTestBasic.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Shelter.UnitTests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Basic()
+		{
+			Assert.Pass();
+		}
+	}
+}
