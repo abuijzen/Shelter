a4ade298539b6f578c72712aa1d0702ad7b63bd8
Angelique <angeliquebuijzen@hotmail.com>
2020-01-14 15:45:49 +0100

    uitleg bij unittest geschreven

--- Shelter.UnitTests/UnitTestUniqueness.cs
+++ Shelter.UnitTests/UnitTestUniqueness.cs
@@ -9,6 +9,7 @@ namespace Shelter.UnitTests
 {
     public class UnitTestUniqueness
     {
+        //objecten maken die het gedrag van de echte objecten simuleert = mocking
         //mock data acces, een nul opbject doorgeven
         private Mock<ShelterDataAccess> _dataAccess = new Mock<ShelterDataAccess>(MockBehavior.Strict, new object[] { null })
         {
@@ -18,11 +19,15 @@ namespace Shelter.UnitTests
         [Test]
         public void Unique_NoAnimalsExistInShelter()
         {
-            _dataAccess.Setup(x => x.GetShelterByIdWithAnimals(12)).Returns(new shared.Shelter()
+            //vraag shelter op via id, en krijg de lijst met dieren
+            _dataAccess.Setup(x => x.GetShelterByIdWithAnimals(2)).Returns(new shared.Shelter()
             {
+                //krijg de lijst met dieren
                 Animals = new List<Animal>()
             });
-            var result = _dataAccess.Object.CheckIfUnique(12, "Name");
+
+            //kijk of id 2, name "Marcel", geen 2x voorkomt
+            var result = _dataAccess.Object.CheckIfUnique(2, "Marcel");
             Assert.IsNull(result); 
             // null == default(Animal)
         }
@@ -30,15 +35,19 @@ namespace Shelter.UnitTests
         [Test]
         public void Unique_AnotherAnimalsExistInShelterButDifferentName()
         {
+            //vraag shelter op met id 2
             _dataAccess.Setup(x => x.GetShelterByIdWithAnimals(12)).Returns(new shared.Shelter()
             {
+                //krijg de lijst met alle animals, voeg er eentje aan toe met naam 'feli'
                 Animals = new List<Animal> {
           new Animal {
-            Name = "abcd"
+            Name = "Feli"
           }
         }
             });
-            var result = _dataAccess.Object.CheckIfUnique(12, "abcdef");
+            //kijk of 2, felix al bestaat
+            //als je hier Feli typt, zal de test falen
+            var result = _dataAccess.Object.CheckIfUnique(12, "Felix");
             Assert.IsNull(result);
         }
     }
